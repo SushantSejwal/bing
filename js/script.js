b926e7275f432f4b2b70cb951a2b13acd3c6b443
Sushant Sejwal <sushant@localhost.localdomain>
2023-04-26 16:19:28 +0530

    updating the hidden message part

--- js/script.js
+++ js/script.js
@@ -1,20 +1,38 @@
 const input = document.getElementById('search-quan-intput');
 const btn = document.getElementById('search-btn');
+const hiddenMessage = document.getElementById('message-text');
 
 btn.addEventListener('click', startSearching);
 
 function startSearching(){
     let inputValue = input.value
-    for (let i = 0; i < inputValue; i++){
-        const wordRequest = new XMLHttpRequest();
-        wordRequest.addEventListener('readystatechange', function(event){
-            if (event.target.readyState === 4 && event.target.status === 200){
-                let word = JSON.parse(event.target.responseText);
-                window.open(`https://www.bing.com/search?q=${word[0].replace(" ", "+")}`, "_blank");
-                // window.open(event.target.responseText)
-            }
-        });
-        wordRequest.open('GET', 'https://random-word-api.herokuapp.com/word');
-        wordRequest.send();
+
+    if(inputValue === ""){
+        hiddenMessage.textContent = 'please enter number';
+        hiddenMessage.style.visibility = 'visible';
+    } else if(inputValue === 0){
+        hiddenMessage.textContent = 'please enter number greater than 0';
+        hiddenMessage.style.visibility = 'visible';
+    } else if(inputValue >= 1 && inputValue <= 50 || inputValue > 50){
+        if (inputValue > 50){
+            input.value = 50;
+            inputValue = 50;
+            hiddenMessage.textContent = 'only 50 searches will be made';
+            hiddenMessage.style.visibility = 'visible';
+        }
+        for (let i = 0; i < inputValue; i++){
+            const wordRequest = new XMLHttpRequest();
+            wordRequest.addEventListener('readystatechange', function(event){
+                if (event.target.readyState === 4 && event.target.status === 200){
+                    let word = JSON.parse(event.target.responseText);
+                    window.open(`https://www.bing.com/search?q=${word[0].replace(" ", "+")}`, "_blank");
+                    // window.open(event.target.responseText)
+                }
+            });
+            wordRequest.open('GET', 'https://random-word-api.herokuapp.com/word');
+            wordRequest.send();
+        }
     }
+    
+    
 }
