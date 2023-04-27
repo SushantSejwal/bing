f241cbf56594e496074020de6a0368206340a7af
Sushant Sejwal <sushant@localhost.localdomain>
2023-04-27 14:07:56 +0530

    changing API URL for better functioning

--- js/script.js
+++ js/script.js
@@ -10,28 +10,32 @@ function startSearching(){
     if(inputValue === ""){
         hiddenMessage.textContent = 'please enter number';
         hiddenMessage.style.visibility = 'visible';
+
     } else if(inputValue === 0){
         hiddenMessage.textContent = 'please enter number greater than 0';
         hiddenMessage.style.visibility = 'visible';
+
     } else if(inputValue >= 1 && inputValue <= 50 || inputValue > 50){
         if (inputValue > 50){
             input.value = 50;
             inputValue = 50;
             hiddenMessage.textContent = 'only 50 searches will be made';
             hiddenMessage.style.visibility = 'visible';
-        }
-        for (let i = 0; i < inputValue; i++){
-            const wordRequest = new XMLHttpRequest();
-            wordRequest.addEventListener('readystatechange', function(event){
-                if (event.target.readyState === 4 && event.target.status === 200){
-                    let word = JSON.parse(event.target.responseText);
-                    window.open(`https://www.bing.com/search?q=${word[0].replace(" ", "+")}`, "_blank");
-                    // window.open(event.target.responseText)
+        }    
+
+        const wordRequest = new XMLHttpRequest();
+
+        wordRequest.addEventListener('readystatechange', function(event){
+            if (event.target.readyState === 4 && event.target.status === 200){
+                let words = JSON.parse(event.target.responseText);
+                for (let i = 0; i < words.length; i++){
+                    window.open(`https://www.bing.com/search?q=${words[i].replace(" ", "+")}`, "_blank");
                 }
-            });
-            wordRequest.open('GET', 'https://random-word-api.herokuapp.com/word');
-            wordRequest.send();
-        }
+            }
+        });
+
+        wordRequest.open('GET', `https://random-word-api.vercel.app/api?words=${inputValue}`);
+        wordRequest.send();
     }
     
     
